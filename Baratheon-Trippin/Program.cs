abdb1219bdad4787dfca0b95eb0808c929a9a2c4
pmpurcell93 <maddenpurcell@gmail.com>
2022-01-30 11:45:00 -0600

    Add project files.

---
+++ Baratheon-Trippin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Baratheon_Trippin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
