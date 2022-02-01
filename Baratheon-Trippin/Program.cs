3f0afc6c244f9801df49ac874e8128e10f251238
pmpurcell93 <maddenpurcell@gmail.com>
2022-01-31 21:45:04 -0600

    Start up menu

--- Baratheon-Trippin/Program.cs
+++ Baratheon-Trippin/Program.cs
@@ -6,7 +6,52 @@ namespace Baratheon_Trippin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ViewMenu();
+                string answer = Console.ReadLine();
+            while (answer != null)
+            {
+
+                switch (answer)
+                {
+                    case "1":
+                        Console.WriteLine("You pressed 1 -- Enter District Sales");
+                        ViewMenu();
+                        answer = Console.ReadLine();
+                        break;
+                    case "2":
+                        Console.WriteLine("You pressed 2 -- Generate District Report");
+                        ViewMenu();
+                        answer = Console.ReadLine();
+                        break;
+                    case "3":
+                        Console.WriteLine("You pressed 3 -- Add New Employee");
+                        ViewMenu();
+                        answer = Console.ReadLine();
+                        break;
+                    case "4":
+                        Console.WriteLine("You pressed 4 -- Add a Store/District");
+                        ViewMenu();
+                        answer = Console.ReadLine();
+                        break;
+                    case "5":
+                        System.Environment.Exit(1);
+                        break;
+                }
+            }
+            }
+
+        private static void ViewMenu()
+        {
+            Console.WriteLine(@"Baratheon QuickTrip
+'Ours is the fury!'
+___________________________
+Select your option:
+1. Enter District Sales
+2. Generate District Report
+3. Add New Employee
+4. Add a Store/District
+5. Exit
+___________________________");
         }
     }
-}
+    }
